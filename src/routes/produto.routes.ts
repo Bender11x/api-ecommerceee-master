6017109b80d958ad1695041b39b3da4512e00677
= <soaresrodrigobender@gmail.com>
2025-07-17 09:36:07 -0300

    testee

---
+++ src/routes/produto.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { ProdutoController } from "../controllers/ProdutoController";
+import { authJwt } from "../middlewares/authJwt";
+
+const routes = Router()
+
+routes.get('/', authJwt,ProdutoController.listar)
+routes.get('/:id',authJwt,ProdutoController.buscar)
+routes.post('/', authJwt,ProdutoController.criar)
+routes.put('/:id', authJwt,ProdutoController.atualizar)
+routes.delete('/:id', authJwt,ProdutoController.deletar)
+
+export default routes
